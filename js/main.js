479f545ebb4342259598c0b06c86af1dbcfa36c8
Steven Daniel⚡️☕️ <162411702+csharptitan@users.noreply.github.com>
2024-08-01 17:16:15 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,21 +56,21 @@ jQuery(function($) {'use strict';
 	      percentTime;
 	 
 	    //Init the carousel
-	    $("#main-slider").find('.owl-carousel').owlCarousel({
-	      slideSpeed : 500,
-	      paginationSpeed : 500,
-	      singleItem : true,
-	      navigation : true,
-			navigationText: [
-			"<i class='fa fa-angle-left'></i>",
-			"<i class='fa fa-angle-right'></i>"
-			],
-	      afterInit : progressBar,
-	      afterMove : moved,
-	      startDragging : pauseOnDragging,
-	      //autoHeight : true,
-	      transitionStyle : "fadeUp"
-	    });
+	  //   $("#main-slider").find('.owl-carousel').owlCarousel({
+	  //     slideSpeed : 500,
+	  //     paginationSpeed : 500,
+	  //     singleItem : true,
+	  //     navigation : true,
+			// navigationText: [
+			// "<i class='fa fa-angle-left'></i>",
+			// "<i class='fa fa-angle-right'></i>"
+			// ],
+	  //     afterInit : progressBar,
+	  //     afterMove : moved,
+	  //     startDragging : pauseOnDragging,
+	  //     //autoHeight : true,
+	  //     transitionStyle : "fadeUp"
+	  //   });
 	 
 	    //Init progressBar where elem is $("#owl-demo")
 	    function progressBar(elem){
